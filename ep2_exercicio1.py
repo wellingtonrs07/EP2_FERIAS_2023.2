e6e6771297381cba9b85d231001e307a2922a462
wellingtonrs07 <wr24349@gmail.com>
2023-07-25 08:55:50 -0300

    sorteio feito

--- ep2_exercicio1.py
+++ ep2_exercicio1.py
@@ -152,4 +152,133 @@ def valida_questoes(lista_questoes):
     
     return lista_retorno
 
-print(valida_questao({'titulo': 'Dentre os listados, qual destes esportes é menos praticado no Brasil?', 'nivel': 'facil', 'opcoes': {'A': ' ', 'B': ' \t', 'C': 'Ski Cross Country', 'D': ' \t\t'}, 'correta': 'C'}))
+#QUESTAO 4
+import random
+def sorteia_questao(questoes, nivel):
+
+    #criando uma lista com o nivel pedido
+    lista_questoes_por_dificuldade = []
+
+    for dificuldade, lista_questao in questoes.items():
+        for questao in lista_questao:
+            if dificuldade == nivel:
+
+                lista_questoes_por_dificuldade.append(questao)
+
+    #sorteando um indice da lista
+    indice_sorteado= random.randint(0, len(lista_questoes_por_dificuldade)-1)
+    questao_sorteada = lista_questoes_por_dificuldade[indice_sorteado]
+
+    return questao_sorteada
+
+questoes = {
+  "facil": [
+    {
+      "titulo": "Qual o resultado da operação 57 + 32?",
+      "nivel": "facil",
+      "opcoes": {
+        "A": "-19",
+        "B": "85",
+        "C": "89",
+        "D": "99"
+      },
+      "correta": "C"
+    },
+    {
+      "titulo": "Qual destes parques não se localiza em São Paulo?!",
+      "nivel": "facil",
+      "opcoes": {
+        "A": "Ibirapuera",
+        "B": "Parque do Carmo",
+        "C": "Parque Villa Lobos",
+        "D": "Morro da Urca"
+      },
+      "correta": "D"
+    },
+    {
+      "titulo": "Qual destas não é uma linguagem de programação?",
+      "nivel": "facil",
+      "opcoes": {
+        "A": "Miratdes",
+        "B": "Python",
+        "C": "Lua",
+        "D": "C++"
+      },
+      "correta": "A"
+    },
+    {
+      "titulo": "Dentre os listados, qual destes esportes é menos praticado no Brasil?",
+      "nivel": "facil",
+      "opcoes": {
+        "A": "Natação",
+        "B": "Vôlei",
+        "C": "Ski Cross Country",
+        "D": "Natação"
+      },
+      "correta": "C"
+    }
+  ],
+  "medio": [
+    {
+      "titulo": "Qual destes números é primo?",
+      "nivel": "medio",
+      "opcoes": {
+        "A": "259",
+        "B": "85",
+        "C": "49",
+        "D": "19"
+      },
+      "correta": "D"
+    },
+    {
+      "titulo": "Na Conjectura de _______, escolhendo-se um número natural inicial n, onde n > 0, os seguintes critérios serão obedecidos: Se n for par o seu sucessor será a metade e se n for ímpar o seu sucessor será o triplo mais um, gerando então um novo número. Qual o nome da conjectura?",
+      "nivel": "medio",
+      "opcoes": {
+        "A": "Collatz",
+        "B": "Goldbach",
+        "C": "Poincaré",
+        "D": "Hodge"
+      },
+      "correta": "A"
+    },
+    {
+      "titulo": "Qual a segunda pessoa mais seguida no Instagram?",
+      "nivel": "medio",
+      "opcoes": {
+        "A": "Cristiano Ronaldo",
+        "B": "Dwayne Johnson",
+        "C": "Kim Kardashian",
+        "D": "Kylie Jenner"
+      },
+      "correta": "D"
+    }
+  ],
+  "dificil": [
+    {
+      "titulo": "A reprodução dos seres vivos é um processo biológico através do qual os organismos geram descendência. Qual desta não é uma forma de reprodução assexuada?",
+      "nivel": "dificil",
+      "opcoes": {
+        "A": "Autogamia",
+        "B": "Esporulação",
+        "C": "Partenogênese",
+        "D": "Divisão binária"
+      },
+      "correta": "A"
+    },
+    {
+      "titulo": "Qual o resultado da operação 5 + 2 * 3 ^ 2, onde ^ representa potenciação?",
+      "nivel": "dificil",
+      "opcoes": {
+        "A": "441",
+        "B": "86",
+        "C": "Nenhuma das outras respostas",
+        "D": "23"
+      },
+      "correta": "D"
+    }
+  ]
+}
+
+nivel = 'facil'
+
+print(sorteia_questao(questoes, nivel))
